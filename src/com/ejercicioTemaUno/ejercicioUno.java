d087da74bffe6e01bee3697077162c4df7653362
manuel18zr <114772594+manuel18zr@users.noreply.github.com>
2022-10-23 20:58:45 +0200

    Initial commit

---
+++ src/com/ejercicioTemaUno/ejercicioUno.java
@@ -0,0 +1,45 @@
+package com.ejercicioTemaUno;
+
+public class ejercicioUno {
+    public static void main(String[] args) {
+
+        //1.numericos
+
+        //1.1 enteros
+        byte variable1 = 5;
+        System.out.println(variable1);
+
+        short variable2 = 10;
+        System.out.println(variable2);
+
+        int variable3 = 15;
+        System.out.println(variable3);
+
+        long variable4 = 1000;
+        System.out.println(variable4);
+
+        //1.2 decimales
+        float variable5 = 5.5f;
+        System.out.println(variable5);
+
+        double variable6 = 2.2d;
+        System.out.println(variable6);
+
+        //booleano
+
+        boolean variable7 = false;
+        System.out.println(variable7);
+
+        boolean variable8 = true;
+        System.out.println(variable8);
+
+        //texto
+
+        char variable9 = 'A';
+        System.out.println(variable9);
+
+        String variable10 = "Nombre";
+        System.out.println(variable10);
+
+    }
+}
